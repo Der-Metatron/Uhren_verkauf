e22909ddd97170384d86607f811f7d887e4c8e96
sprotte <metatron1972@live.de>
2022-01-18 12:57:27 +0100

    sprotte

---
+++ README.md
@@ -0,0 +1,38 @@
+# javaScript-mouseEvents
+
+## Aufgabenstellung
+Deine Aufgabe ist es, einen Online Shop zu bauen. 
+
+Es soll sich um ein Mode-Uhrengeschäft handeln.
+
+Dein Geschäft hat den Namen QueTiempo und bietet 10 Uhren an. 
+
+## Tech Stack
+Beginne mit HTML und CSS
+
+Gerne darfst du Bootstrap und Sass benutzen.
+
+## Seitenaufbau
+Deine Seite muss mindestens einen Header mit einem Logo und eine Hauptseite mit den 10 Produkten beinhalten.
+
+Jedes Produkt hat ein Bild und einen Button als Kaufaktion. 
+
+Entscheide selber was passiert, wenn ein Nutzer auf den Button klickt.
+
+Zusätzlich muss jedes Produkt einen Namen haben. 
+
+Der Name ist von dir frei wählbar.
+
+## Ressourcen
+Neben dieser Anleitung musst du selber nach einem Logo und nach Fotos suchen.
+
+Recherchiere auf Shutterstock und ähnlichen Seiten nach frei verfügbaren Fotoaufnahmen.
+
+Erstelle einen Branch mit deinem Namen und lade dort deine Ergebnisse hoch.
+
+## MouseEvent
+Falls ein Nutzer mit der Maus über eines deiner Bilder fliegt, soll sich das Bild auf 500px Breite und 500px Höhe vergrößern.
+
+Wenn er mit der Maus anschließend aus dem Bild geht, verkleinert sich das Bild auf 100px Breite und 100 px Höhe.
+
+Standardmäßig müssen alle Bilder eine Breite von 100px und eine Höhe von 100px besitzen.
---
+++ index.js
@@ -0,0 +1,91 @@
+"use strict";
+
+function uhr1() {
+  document.getElementById("uhr").style.width = "500px";
+  document.getElementById("uhr").style.height = "500px";
+}
+function uhrback1() {
+  document.getElementById("uhr").style.width = "250px";
+  document.getElementById("uhr").style.height = "250px";
+}
+
+function uhr2() {
+  document.getElementById("uhr2").style.width = "500px";
+  document.getElementById("uhr2").style.height = "500px";
+}
+function uhrback2() {
+  document.getElementById("uhr2").style.width = "250px";
+  document.getElementById("uhr2").style.height = "250px";
+}
+
+function uhr3() {
+  document.getElementById("uh3").style.width = "500px";
+  document.getElementById("uhr3").style.height = "500px";
+}
+function uhrback3() {
+  document.getElementById("uhr3").style.width = "250px";
+  document.getElementById("uhr3").style.height = "250px";
+}
+
+function uhr4() {
+  document.getElementById("uhr4").style.width = "500px";
+  document.getElementById("uhr4").style.height = "500px";
+}
+function uhrback4() {
+  document.getElementById("uhr4").style.width = "250px";
+  document.getElementById("uhr4").style.height = "250px";
+}
+
+function uhr5() {
+  document.getElementById("uhr5").style.width = "500px";
+  document.getElementById("uhr5").style.height = "500px";
+}
+function uhrback5() {
+  document.getElementById("uhr5").style.width = "250px";
+  document.getElementById("uhr5").style.height = "250px";
+}
+
+function uhr6() {
+  document.getElementById("uhr6").style.width = "500px";
+  document.getElementById("uhr6").style.height = "500px";
+}
+function uhrback6() {
+  document.getElementById("uhr6").style.width = "250px";
+  document.getElementById("uhr6").style.height = "250px";
+}
+
+function uhr7() {
+  document.getElementById("uhr7").style.width = "500px";
+  document.getElementById("uhr7").style.height = "500px";
+}
+function uhrback7() {
+  document.getElementById("uhr7").style.width = "250px";
+  document.getElementById("uhr7").style.height = "250px";
+}
+
+function uhr8() {
+  document.getElementById("uhr8").style.width = "500px";
+  document.getElementById("uhr8").style.height = "500px";
+}
+function uhrback8() {
+  document.getElementById("uhr8").style.width = "250px";
+  document.getElementById("uhr8").style.height = "250px";
+}
+
+function uhr9() {
+  document.getElementById("uhr9").style.width = "500px";
+  document.getElementById("uhr9").style.height = "500px";
+}
+function uhrback9() {
+  document.getElementById("uhr9").style.width = "250px";
+  document.getElementById("uhr9").style.height = "250px";
+}
+
+function uhr10() {
+  document.getElementById("uhr10").style.width = "500px";
+  document.getElementById("uhr10").style.height = "500px";
+}
+function uhrback10() {
+  document.getElementById("uhr10").style.width = "250px";
+  document.getElementById("uhr10").style.height = "250px";
+}
